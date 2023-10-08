a849142fc452ccc1ad351d0d64f170b14035a8b3
nullsploit01 <harshaldharmik099@gmail.com>
2023-10-08 12:41:43 +0530

    send auth token via header

--- src/clients/apollo.client.ts
+++ src/clients/apollo.client.ts
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
 import { Environment } from 'src/config/environment.config'
+import { storageKeys } from 'src/constants/storage.constants'
+import { storageService } from 'src/services/storage.service'
+
+const httpLink = createHttpLink({
+  uri: Environment.ServerUrl
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  const { token } = await storageService.get(storageKeys.auth.token)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `${token}` : ''
+    }
+  }
+})
 
 export const apolloClient = new ApolloClient({
-  uri: Environment.ServerUrl,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
--- src/components/pages/auth/register/register.page.tsx
+++ src/components/pages/auth/register/register.page.tsx
@@ -123,7 +123,7 @@ const styles = StyleSheet.create({
     paddingVertical: 80
   },
   input: {
-    marginBottom: 26
+    marginBottom: 20
   },
   seperator: {
     flexDirection: 'row',
